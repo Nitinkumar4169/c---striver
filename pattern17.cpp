a477d154c74664ea560df0607ee3ecd4f5be3e83
Nitin <nitinkmr.4169@gmail.com>
2025-07-27 11:12:19 +0530

    pattern1

---
+++ pattern17.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void print17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // characters
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (int j = 0; j <= 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint)
+                ch++;
+            else
+                ch--;
+        }
+
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        print17(n);
+    }
+}
